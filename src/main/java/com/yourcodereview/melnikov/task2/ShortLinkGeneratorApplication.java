827cfef9a62dea062c372dc0db70038b52c7384b
Alexantal <Alexantal@yandex.ru>
2022-05-30 14:51:06 +0300

    Initial commit

---
+++ src/main/java/com/yourcodereview/melnikov/task2/ShortLinkGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.yourcodereview.melnikov.task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yourcodereview/melnikov/task2/ShortLinkGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcodereview.melnikov.task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
